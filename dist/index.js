d43decf5c67e2a92e4b55db6286235cb0541738a
Lokesh Poovaragan <dude.1996@live.com>
2022-11-09 05:29:56 +0000

    wip upload

--- dist/index.js
+++ dist/index.js
@@ -12556,6 +12556,102 @@ module.exports = require("net");
 
 /***/ }),
 
+/***/ 2254:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("node:buffer");
+
+/***/ }),
+
+/***/ 7561:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("node:fs");
+
+/***/ }),
+
+/***/ 8849:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("node:http");
+
+/***/ }),
+
+/***/ 2286:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("node:https");
+
+/***/ }),
+
+/***/ 7503:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("node:net");
+
+/***/ }),
+
+/***/ 9411:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("node:path");
+
+/***/ }),
+
+/***/ 2571:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("node:process");
+
+/***/ }),
+
+/***/ 4492:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("node:stream");
+
+/***/ }),
+
+/***/ 2477:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("node:stream/web");
+
+/***/ }),
+
+/***/ 1041:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("node:url");
+
+/***/ }),
+
+/***/ 7261:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("node:util");
+
+/***/ }),
+
+/***/ 5628:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("node:zlib");
+
+/***/ }),
+
 /***/ 2037:
 /***/ ((module) => {
 
@@ -12617,6 +12713,14 @@ module.exports = require("url");
 
 "use strict";
 module.exports = require("util");
+
+/***/ }),
+
+/***/ 1267:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("worker_threads");
 
 /***/ }),
 
@@ -12732,10 +12836,105 @@ module.exports = JSON.parse('[[[0,44],"disallowed_STD3_valid"],[[45,46],"valid"]
 /******/ 		return module.exports;
 /******/ 	}
 /******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__nccwpck_require__.m = __webpack_modules__;
+/******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__nccwpck_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__nccwpck_require__.o(definition, key) && !__nccwpck_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/ensure chunk */
+/******/ 	(() => {
+/******/ 		__nccwpck_require__.f = {};
+/******/ 		// This file contains only the entry chunk.
+/******/ 		// The chunk loading function for additional chunks
+/******/ 		__nccwpck_require__.e = (chunkId) => {
+/******/ 			return Promise.all(Object.keys(__nccwpck_require__.f).reduce((promises, key) => {
+/******/ 				__nccwpck_require__.f[key](chunkId, promises);
+/******/ 				return promises;
+/******/ 			}, []));
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/get javascript chunk filename */
+/******/ 	(() => {
+/******/ 		// This function allow to reference async chunks
+/******/ 		__nccwpck_require__.u = (chunkId) => {
+/******/ 			// return url for filenames based on template
+/******/ 			return "" + chunkId + ".index.js";
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__nccwpck_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__nccwpck_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat */
 /******/ 	
 /******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/******/ 	/* webpack/runtime/require chunk loading */
+/******/ 	(() => {
+/******/ 		// no baseURI
+/******/ 		
+/******/ 		// object to store loaded chunks
+/******/ 		// "1" means "loaded", otherwise not loaded yet
+/******/ 		var installedChunks = {
+/******/ 			179: 1
+/******/ 		};
+/******/ 		
+/******/ 		// no on chunks loaded
+/******/ 		
+/******/ 		var installChunk = (chunk) => {
+/******/ 			var moreModules = chunk.modules, chunkIds = chunk.ids, runtime = chunk.runtime;
+/******/ 			for(var moduleId in moreModules) {
+/******/ 				if(__nccwpck_require__.o(moreModules, moduleId)) {
+/******/ 					__nccwpck_require__.m[moduleId] = moreModules[moduleId];
+/******/ 				}
+/******/ 			}
+/******/ 			if(runtime) runtime(__nccwpck_require__);
+/******/ 			for(var i = 0; i < chunkIds.length; i++)
+/******/ 				installedChunks[chunkIds[i]] = 1;
+/******/ 		
+/******/ 		};
+/******/ 		
+/******/ 		// require() chunk loading for javascript
+/******/ 		__nccwpck_require__.f.require = (chunkId, promises) => {
+/******/ 			// "1" is the signal for "already loaded"
+/******/ 			if(!installedChunks[chunkId]) {
+/******/ 				if(true) { // all chunks have JS
+/******/ 					installChunk(require("./" + __nccwpck_require__.u(chunkId)));
+/******/ 				} else installedChunks[chunkId] = 1;
+/******/ 			}
+/******/ 		};
+/******/ 		
+/******/ 		// no external install chunk
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 	})();
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
@@ -12743,19 +12942,201 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(2293);
 const github = __nccwpck_require__(33);
+const fetch = (...args) => __nccwpck_require__.e(/* import() */ 159).then(__nccwpck_require__.bind(__nccwpck_require__, 7159)).then(({default: fetch}) => fetch(...args));
+const path = __nccwpck_require__(1017);
+const fs = __nccwpck_require__(7147);
+const spheronBaseUrl = "https://api-v2.spheron.network"
+
+async function getScope(apiKey) {
+
+//authHeaders = new fetch.Headers()
+//authHeaders.append("Authorization", `Bearer ${apiKey}`)
+
+var requestOptions = {
+  method: 'GET',
+  headers: {
+    "Authorization": `Bearer ${apiKey}`
+  },
+  redirect: 'follow'
+};
+
+
+const resp = await fetch(`${spheronBaseUrl}/v1/api-keys/scope`, requestOptions)
+const respJson = await resp.json()
+
+return respJson
+
+}
+
+function readFilesSync(dir) {
+  const files = [];
+
+  fs.readdirSync(dir).forEach(filename => {
+    const name = path.parse(filename).name;
+    const ext = path.parse(filename).ext;
+    const filepath = path.resolve(dir, filename);
+    const stat = fs.statSync(filepath);
+    const isFile = stat.isFile();
+
+    if (isFile) files.push({ filepath, name, ext, stat });
+  });
+
+  files.sort((a, b) => {
+    // natural sort alphanumeric strings
+    // https://stackoverflow.com/a/38641281
+    return a.name.localeCompare(b.name, undefined, { numeric: true, sensitivity: 'base' });
+  });
+
+  console.log(files)
+
+  return files;
+}
+
+async function postFiles(apiKey, buildFolder, organizationId, projectName, protocol) {
+
+//var myHeaders = new Headers();
+//myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlLZXkiOiI2YjczNTBjZjUzZDVkZmNmY2ViY2VjOTVjOGY5OWU0YzUwZmQ5OGMzMWFjMzc3MzY4ODYxODRjMzE4NzFiNjA5ZjlhNTliYzA3OGYwNjllZTg0MWVhOTUxZWRjNzFhNTc3YjQyYTNhZGE1NjJmYmYwOGNhOWEzNWIzMjJiMmUxOSIsImlhdCI6MTY2NTQwOTMxNywiaXNzIjoid3d3LnNwaGVyb24ubmV0d29yayJ9.UFPHCaxb2wvUPmz5SbQNKT-19Yy7XdT38SHbvY6wZkE");
+//myHeaders.append("Content-Type", "application/json");
+
+//var raw = JSON.stringify({
+//  "organizationId": "string",
+//  "gitUrl": "string",
+//  "repoName": "string",
+//  "uniqueTopicId": "string",
+//  "configuration": {
+//    "buildCommand": "string",
+//    "installCommand": "string",
+//    "workspace": "string",
+//    "publishDir": "string",
+//    "framework": "static",
+//    "nodeVersion": "V_12"
+//  },
+//  "env": {},
+//  "protocol": "string",
+//  "createDefaultWebhook": true,
+//  "provider": "string",
+//  "branch": "string"
+//});
+//files.forEach((file) => {
+  //console.log(file)
+//})
+console.log('buildFolder', buildFolder)
+syncedFiles = readFilesSync(buildFolder)
+//console.log(syncedFiles)
+// const filesFormData = {
+//   files: syncedFiles,
+// };
+
+var requestOptions = {
+  method: 'POST',
+  headers: {
+    "Authorization": `Bearer ${apiKey}`
+  },
+  body: syncedFiles,
+  redirect: 'follow'
+};
+
+console.log('organization', organizationId)
+console.log('project', projectName)
+console.log('protocol', protocol)
+searchParams = new URLSearchParams({
+	organization: organizationId,
+	project: projectName,
+	protocol: protocol
+})
+console.log(searchParams)
+const resp = await fetch(`${spheronBaseUrl}/v1/deployment/upload?${searchParams}`, requestOptions)
+const respJson = await resp.json()
+
+return respJson
+
+//fetch("https://api-v2.spheron.network/v1/deployment", requestOptions)
+  //.then(response => response.text())
+  //.then(result => console.log(result))
+  //.catch(error => console.log('error', error));
+
+}
+
+var _getFiles = function(dir) {
+
+    var results = [];
+
+    filesystem.readdirSync(dir).forEach(function(file) {
+
+        file = dir+'/'+file;
+        var stat = filesystem.statSync(file);
+
+        if (stat && stat.isDirectory()) {
+            results = results.concat(_getFiles(file))
+        } else results.push(file);
+
+    });
+
+    return results;
+
+};
 
 try {
   // `who-to-greet` input defined in action metadata file
   //const nameToGreet = core.getInput('who-to-greet');
   //console.log(`Hello ${nameToGreet}!`);
-  const spheronApiKey = core.getInput('spheron-api-key')
+  //const spheronApiKey = core.getInput('spheron-api-key')
+  const spheronApiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlLZXkiOiI2YjczNTBjZjUzZDVkZmNmY2ViY2VjOTVjOGY5OWU0YzUwZmQ5OGMzMWFjMzc3MzY4ODYxODRjMzE4NzFiNjA5ZjlhNTliYzA3OGYwNjllZTg0MWVhOTUxZWRjNzFhNTc3YjQyYTNhZGE1NjJmYmYwOGNhOWEzNWIzMjJiMmUxOSIsImlhdCI6MTY2NTQwOTMxNywiaXNzIjoid3d3LnNwaGVyb24ubmV0d29yayJ9.UFPHCaxb2wvUPmz5SbQNKT-19Yy7XdT38SHbvY6wZkE"
   console.log(`Spheron API Key: ${spheronApiKey}`)
-  const spheronProjectName = core.getInput('spheron-project-name')
+  //const spheronProjectName = core.getInput('spheron-project-name')
+  const spheronProjectName = "Dragd"
   console.log(`Spheron Project Name: ${spheronProjectName}`)
-  const spheronOrganizationId = core.getInput('spheronOrganizationId')
+  //const spheronOrganizationId = core.getInput('spheronOrganizationId')
+  let spheronOrganizationId = ''
   console.log(`Spheron Organization ID: ${spheronOrganizationId}`)
+  //let spheronUploadFolder = core.getInput('spheron-upload-folder')
+  const spheronUploadFolder = 'dist'
+  console.log(`Spheron Upload Folder: ${spheronUploadFolder}`)
+  const spheronProtocol = 'ipfs-pinata'
 
+  if(spheronOrganizationId == ''){
+    //const scope = await getScope(spheronApiKey)
+    //spheronOrganizationId = res['organizations'][0]["id"]
+    getScope(spheronApiKey).then((res) => {
+      spheronOrganizationId = res['organizations'][0]["id"]
+      console.log('spheronOrganizationId', spheronOrganizationId)
+      const directoryPath = path.join(__dirname, spheronUploadFolder)
+      files = readFilesSync(directoryPath)
+      console.log(files)
+	  uploaded = postFiles(spheronApiKey, directoryPath, spheronOrganizationId, spheronProjectName, spheronProtocol)
+	  uploaded.then((res) => {
+	  	console.log('uploaded', res)
+	  })
+      //fs.readdir(directoryPath, async function (err, files) {
+      	//if(err){
+      	//	return console.log(`Unable to scan directory`, err);
+      	//}
+      	//uploaded = await postFiles(spheronApiKey, directoryPath, spheronOrganizationId, spheronProjectName, spheronProtocol)
+		//console.log(uploaded)      	      	
+      //})
+    })
+  }
 
+  //joining path of directory 
+  //const directoryPath = path.join(__dirname, spheronUploadFolder);
+  //passsing directoryPath and callback function
+  //fs.readdir(directoryPath, async function (err, files) {
+      //handling error
+      //if (err) {
+      //    return console.log('Unable to scan directory: ' + err);
+      //} 
+      //listing all files using forEach
+      //files.forEach(function (file) {
+          // Do whatever you want to do with the file
+          //console.log(file); 
+      //});
+      //uploadedJson = await postFiles(spheronApiKey, files, spheronOrganizationId, spheronProjectName, spheronProtocol)
+      //uploadedJson = await postFiles(spheronApiKey, files, spheronOrganizationId, spheronProjectName, spheronProtocol)
+      //console.log(uploadedJson)
+      //uploadedJson.then((res) => {
+      //	console.log(res)
+      //})
+  //});
   // const time = (new Date()).toTimeString();
   // core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
